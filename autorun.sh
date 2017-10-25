63922ddf6dc8b2f987790e3715bda809bd20581b
Gary Feng <gary.feng@gmail.com>
2017-10-25 00:02:57 -0400

    fixing autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# instructions on setting up a demo kp
-
-# setting up the git
+# env
+cd ./example_repo/
 git remote add origin https://github.com/garyfeng/knowledge_repo.git
+
 git config user.name "Gary Feng"
 git config user.email "gary.feng@gmail.com"
 git commit -m initial
-# pull from the github
 git pull https://garyfeng@github.com/garyfeng/knowledge_repo.git
+cd ..
 
-# start a new post with random number added to a static post
+# start a new post
 affix=${RANDOM:0:10}
 knowledge_repo --repo ./example_repo create ipynb example_post_$affix.ipynb
 knowledge_repo --repo ./example_repo add example_post_$affix.ipynb -p project/example_ipynb_$affix
@@ -23,6 +23,6 @@ knowledge_repo --repo ./example_repo submit project/example_ipynb_$affix.kp
 # now we need to do a pull request and merge on the github
 
 
-# serving up the knowledge repo
+
 knowledge_repo --repo ./example_repo runserver
 
